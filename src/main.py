16f9410c96f570fe68303ff25b035e2b3fc78cd7
Kyuma23 <a106920@uminho.pt>
2026-04-20 15:17:10 +0100

    lexer

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from lexer import lexer 
+
+if __name__ == '__main__':
+    test_code = '''
+    PROGRAM TESTE
+        ! Declaracao de variaveis
+        INTEGER A, B
+        REAL C
+        
+        A = 10
+        B = 20
+        C = 3.14
+        
+        ! Controlo de fluxo e operadores relacionais
+        IF (A .LT. B .AND. .TRUE.) THEN
+            PRINT *, 'A e menor que B'
+            CALL MINHASUB(A)
+        ENDIF
+    END
+    '''
+    
+    print("A iniciar análise léxica...\n")
+    lexer.input(test_code)
+    for tok in lexer:
+        print(f"Token: {tok.type:12} | Valor: {tok.value}")
